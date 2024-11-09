4b490d097a974d36602aa2e7028d660bfd2d86aa
hung.nq <hung.nq@vlinkgroup.net>
2024-11-09 22:29:29 +0700

    add div header and footer

---
+++ app-demo/src/app/components/ssr/header.tsx
@@ -0,0 +1,20 @@
+export default function Header () {
+  return(<div className="row-auto">
+    <div className="logo">logo</div>
+    <nav>
+      <ul>
+        <li><a>Home</a></li>
+        <li>
+          <a>Category</a>
+        </li>
+        <li>
+          <a>Contract</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  );
+}
